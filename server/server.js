dab3b8d737cd7fedb5a77bc082446166e730c26f
Siddhartha Sarkar <72632615+Siddharthasid@users.noreply.github.com>
2026-03-30 20:12:05 +0000

    docker and ci setup with github actions

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT ? process.env.PORT : 5000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Node js app, with CI basics' });
+});
+
+app.get('/learn', (req, res) => {
+  res.send('Node js app, with CI basics');
+});
+
+app.listen(port, () => {
+  console.log(`Node Express app listening on port ${port}`);
+});
